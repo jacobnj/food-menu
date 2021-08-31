5c3b1f15f1237935604d5455501b4d3b4764dcd6
jacob <jacobnj@yahoo.com>
2021-08-31 19:27:05 +0530

    first

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/starters">Starters</a>
+                <a href="/maincourse">Main Course</a>
+                <a href="/deserts">Deserts</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
